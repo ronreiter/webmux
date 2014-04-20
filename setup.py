e97fbf533db7a97f2d01243e493efe7fc931a655
Ron Reiter <ron.reiter@gmail.com>
2014-04-20 19:12:10 +0300

    manifest

--- setup.py
+++ setup.py
@@ -30,24 +30,5 @@ setup(
     packages=['webmux'],
     scripts=['webmuxd'],
     license='MIT',
-
-    # TODO: how do you use requirements.txt without moving it into the package?
-    install_requires=[
-        'Twisted==13.2.0',
-        'cffi==0.8.1',
-        'cryptography==0.2.1',
-        'futures==2.1.6',
-        'parse==1.4.1',
-        'pyOpenSSL==0.14',
-        'pyasn1==0.1.7',
-        'pycparser==2.10',
-        'pycrypto==2.6.1',
-        'six==1.5.2',
-        'stuf==0.9.4',
-        'txsockjs==1.2.1',
-        'wsgiref==0.1.2',
-        'zope.interface==4.1.0',
-        'SQLObject==1.5.1',
-        'Jinja2==2.7.2',
-    ]
+    install_requires=reqs
 )
